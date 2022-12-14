7401bb3a4571d9edacdbc2ffac87957b3cbd98cb
Niks Hariman <nhariman@hotmail.com>
2022-12-14 18:33:28 +0100

    still can't figure it out

--- 07/directories.py
+++ 07/directories.py
@@ -1,18 +1,3 @@
-
-def	traverse(data, i, size, size_answer):
-	if (len(data[i]) == 0 ):
-		return size_answer
-	command = data[i].split(" ")
-	if (command[1] == "cd" and command[2] == ".."):
-		if (size <= 100000):
-			size_answer += size
-		return size_answer
-	elif (command[0].isdigit() == True):
-		size += int(command[0])
-	size = traverse(data, i + 1, size, size_answer)
-	return size_answer
-	
-
 # read in data
 file_obj = open("07.txt", "r")
 data = file_obj.readlines()
@@ -20,18 +5,29 @@ data = file_obj.readlines()
 for i in range(len(data)):
 	data[i] = data[i].strip()
 
-# make file_structure list that contains directories and the size 
-# [ "directory_name", "sub_directories", "size"]
-# maybe make this a dictionary?
-# cd [letters] = new recursion call
-# cd .. = return recursion call
-# ls ignore?
-# dir = create list with first element 
+size_answer = 0
 
-size_answer = traverse(data, 0, 0, 0)
+folder_sizes = []
+total_folder_size = 0
+for i in range(len(data)):
+	command = data[i].split(" ") # split command into tokens
+	print(command)
+	if ((len(command) == 3 and command[2] == ".." and len(folder_sizes) > 0) or
+		i == len(data) - 1):
+		for j in range(len(folder_sizes)):
+			total_folder_size += folder_sizes[j]
+		if (total_folder_size <= 100000):
+			size_answer += total_folder_size
+		print("Folder sizes:")
+		print(folder_sizes)
+		print("size:")
+		print(size_answer)
+		total_folder_size = 0
+		folder_sizes = []
+	elif (command[0].isdigit() == True):
+		folder_sizes.append(int(command[0]))
 
 print("puzzle answer:")
 print(size_answer)
 
-
-
+# not 1276418 ?
